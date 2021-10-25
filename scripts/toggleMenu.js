1cc9a681e50a5cbf900d6e057778f09ce7714fc6
Vlad R <romunuk@gmail.com>
2021-10-25 12:45:51 +0300

    failed toggle menu: need rework:

---
+++ scripts/toggleMenu.js
@@ -0,0 +1,21 @@
+
+
+// var menuList = document.getElementById("menuList");
+
+
+// menuList.style.maxHeight = "0px";
+
+// console.log(menuList.style.maxHeight)
+
+// function togglemenu(){
+    
+ 
+//     if(menuList.style.maxHeight == "0px"){
+        
+//         menuList.style.maxHeight = "130px;"
+//     }
+//     else{
+//         menuList.style.maxHeight = "0px";
+//         onsole.log( 123)
+//     }
+// }
