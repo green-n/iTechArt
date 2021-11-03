ef95e3bdea39a1eb284a5994ae26e8a1c4b9c4f4
Vlad R <romunuk@gmail.com>
2021-11-03 17:42:39 +0300

    table page

--- scripts/toggleMenu.js
+++ scripts/toggleMenu.js
@@ -2,9 +2,10 @@
 
 var menuList = document.getElementById("menuList");
 
+if(menuList.style.maxHeight >= "10px"){
 
 menuList.style.maxHeight = "0px";
-menuList.style.visibility="hidden"
+menuList.style.visibility="hidden"}
 
 console.log(menuList.style.maxHeight)
 
@@ -15,7 +16,7 @@ function togglemenu(){
     
         menuList.style.maxHeight = "130px";
         menuList.style.visibility="visible"
-        console.log( 123)
+       console.log( 123 )
     }
     else{
         menuList.style.maxHeight = "0px";
